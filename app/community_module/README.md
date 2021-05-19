2967af81e3e26ad9ba4f2c69698eeb200a744781
frankymjlao <franky.mj.lao@gmail.com>
2021-05-19 08:06:12 +0000

    fiori ui is ready
    Signed-off-by: frankymjlao <franky.mj.lao@gmail.com>

---
+++ app/community_module/README.md
@@ -0,0 +1,32 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Wed May 19 2021 08:00:01 GMT+0000 (Coordinated Universal Time)|
+|**App Generator**<br>@sap/generator-fiori|
+|**App Generator Version**<br>1.1.10|
+|**Generation Platform**<br>SAP Business Application Studio|
+|**Floorplan Used**<br>3worklistodatav4|
+|**Service Type**<br>Local Cap|
+|**Service URL**<br>/raas-data/
+|**Module Name**<br>community_module|
+|**Application Title**<br>Recommended Community Blogs v1.0|
+|**Namespace**<br>|
+|**UI5 Theme**<br>sap_fiori_3_dark|
+|**UI5 Version**<br>Latest|
+|**Enable Telemetry**<br>True|
+
+## community_module
+
+Fiori application for community blogs
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  In order to launch the generated app, simply start your CAP project and navigate to the following location in your browser:
+
+http://localhost:4004/community_module/webapp/index.html
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ app/community_module/webapp/test/flpSandbox.js
@@ -0,0 +1,100 @@
+sap.ui.define([
+	"sap/base/util/ObjectPath",
+	"sap/ushell/services/Container"
+], function (ObjectPath) {
+	"use strict";
+
+	// define ushell config
+	ObjectPath.set(["sap-ushell-config"], {
+		defaultRenderer: "fiori2",
+		bootstrapPlugins: {
+			"RuntimeAuthoringPlugin": {
+				component: "sap.ushell.plugins.rta",
+				config: {
+					validateAppVersion: false
+				}
+			},
+			"PersonalizePlugin": {
+				component: "sap.ushell.plugins.rta-personalize",
+				config: {
+					validateAppVersion: false
+				}
+			}
+		},
+		renderers: {
+			fiori2: {
+				componentData: {
+					config: {
+						enableSearch: false,
+						rootIntent: "Shell-home"
+					}
+				}
+			}
+		},
+		services: {
+			"LaunchPage": {
+				"adapter": {
+					"config": {
+						"groups": [{
+							"tiles": [{
+								"tileType": "sap.ushell.ui.tile.StaticTile",
+								"properties": {
+									"title": "Recommended Community Blogs v1.0",
+									"targetURL": "#RecommendedCommunityBlogsv10-display"
+								}
+							}]
+						}]
+					}
+				}
+			},
+			"ClientSideTargetResolution": {
+				"adapter": {
+					"config": {
+						"inbounds": {
+							"RecommendedCommunityBlogsv10-display": {
+								"semanticObject": "RecommendedCommunityBlogsv10",
+								"action": "display",
+								"description": "Fiori application for community blogs",
+								"title": "Recommended Community Blogs v1.0",
+								"signature": {
+									"parameters": {}
+								},
+								"resolutionResult": {
+									"applicationType": "SAPUI5",
+									"additionalInformation": "SAPUI5.Component=communitymodule",
+									"url": sap.ui.require.toUrl("communitymodule")
+								}
+							}
+						}
+					}
+				}
+			},
+			NavTargetResolution: {
+				config: {
+					"enableClientSideTargetResolution": true
+				}
+			}
+		}
+	});
+
+	var oFlpSandbox = {
+		init: function () {
+			/**
+			 * Initializes the FLP sandbox
+			 * @returns {Promise} a promise that is resolved when the sandbox bootstrap has finished
+			 */
+
+			// sandbox is a singleton, so we can start it only once
+			if (!this._oBootstrapFinished) {
+				this._oBootstrapFinished = sap.ushell.bootstrap("local");
+				this._oBootstrapFinished.then(function () {
+					sap.ushell.Container.createRenderer().placeAt("content");
+				});
+			}
+
+			return this._oBootstrapFinished;
+		}
+	};
+
+	return oFlpSandbox;
+});
